360e68c398bd1b84e692d8e6bb47cf6277afeda1
Debbie <deborahgwenevere@gmail.com>
2019-06-06 12:01:42 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,28 @@
+## Plot 3 
+  ## Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
+  ## which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
+  ## Which have seen increases in emissions from 1999–2008? 
+  ## Use the ggplot2 plotting system to make a plot answer this question.
+      
+  ## Download Data
+      library("data.table")
+      path <- getwd()
+      download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip",                       sep = "/"))
+      unzip(zipfile = "dataFiles.zip")
+      scc <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+      nei <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+  
+  ## In Baltimore city - four sources with most decrease
+      baltimore <- subset (nei, fips == "24510")
+      pmdata <- tapply(baltimore$Emissions, baltimore$year,baltimore$type, sum)
+      library(dplyr)
+      baltimore_pmdata <-summarise(group_by(filter(baltimore), year,type), Emissions=sum(Emissions))
+      library(ggplot2)
+      g <- ggplot(data=baltimore_pmdata, aes(x=factor(year), fill=type, y=Emissions)) + geom_bar(stat= "identity") + facet_grid(.~type) +            xlab("Year") + ylab("PM2.5 Emissions (tons)") + ggtitle ("Baltimore PM2.5 Emissions by Year and Type") +       scale_fill_brewer(palette = "Dark2")     
+ 
+ 
+   ## Saving Plot 3 as a .png file
+      png("plot3.png", width=600, height=600)
+      g <- ggplot(data=baltimore_pmdata, aes(x=factor(year), fill=type, y=Emissions)) + geom_bar(stat= "identity") + facet_grid(.~type) +            xlab("Year") + ylab("PM2.5 Emissions (tons)") + ggtitle ("Baltimore PM2.5 Emissions by Year and Type") +       scale_fill_brewer(palette = "Dark2")  
+      print(g)
+      dev.off()
